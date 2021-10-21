b3f2b6c0c435a7b16212961e90f9e466440c8592
zakaria <zakariasamy10@gmail.com>
2021-10-21 17:43:45 +0200

    Clean the code more

--- notes.js
+++ notes.js
@@ -2,37 +2,31 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-const addNote = function(title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes()
+    const duplicateNote = notes.find((note) => note.title === title)
 
-    // Check if the new title exists in prvious Notes
-    const duplicateNotes = notes.filter(function(note) {
-        return note.title === title
-    })
-
-    if (duplicateNotes.length === 0) {
+    if (!duplicateNote) {
         notes.push({
             title: title,
             body: body
         })
         saveNotes(notes)
-        console.log(chalk.green.inverse('Note added Successfully!'))
+        console.log(chalk.green.inverse('New note added!'))
     } else {
         console.log(chalk.red.inverse('Note title taken!'))
     }
 }
 
-const removeNote = function(title) {
+const removeNote = (title) => {
     const notes = loadNotes()
-    const notesToKeep = notes.filter(function(note) {
-        return note.title !== title
-    })
+    const notesToKeep = notes.filter((note) => note.title !== title)
 
     if (notes.length > notesToKeep.length) {
         console.log(chalk.green.inverse('Note removed!'))
         saveNotes(notesToKeep)
     } else {
-        console.log(chalk.red.inverse('No Note found!'))
+        console.log(chalk.red.inverse('No note found!'))
     }
 }
 
